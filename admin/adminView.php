d111e9342b248159d9010c5ded67fdfdd5b9c55c
ArisKatsarakis <ariskatsarakis@hotmail.com>
2023-11-16 22:31:22 +0200

    Changed adminView to a class

--- admin/adminView.php
+++ admin/adminView.php
@@ -1,7 +1,32 @@
 <?php
-?>
-<div class="wrap">
+/**
+ * 
+ * <!-- <div class="wrap">
 	<div id="asset-admin">
 		Hello from admin
 	</div>
-</div>
+	</div> -->
+ * 
+ * 
+ */
+
+defined( 'ABSPATH' ) or die( 'Hey, you can go away ?' );
+class AdminView {
+	function __construct()
+	{
+		?>
+		<div class="wrap">
+			<div id="asset-admin">
+				Hello from admin
+			</div>
+		</div
+		<?php
+		
+	}
+
+}
+
+if ( class_exists('AdminView') ) {
+	$adminPlugin = new AdminView();
+}
+

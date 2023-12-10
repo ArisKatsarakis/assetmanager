16c80777ac993a082e3216a894715015134cc0f0
ArisKatsarakis <ariskatsarakis@hotmail.com>
2023-12-10 12:46:00 +0200

    Refactored AssetRepository and added Customer Entity as a class

---
+++ Entities/Customer.php
@@ -0,0 +1,19 @@
+<?php
+namespace Ent;
+class Customer {
+    public static function create_customer_table() {
+        global $wpdb;
+        $charsetCollate = $wpdb->get_charset_collate();
+        $clientsTableName = $wpdb->prefix . 'customers';
+        $sql = "CREATE TABLE $clientsTableName (
+            company_id      integer NOT NULL AUTO_INCREMENT,
+            company_name    varchar(255),
+            company_email   varchar(255),
+            company_afm     varchar(10),
+            comapny_address varchar(255), 
+            PRIMARY KEY (company_id)
+        ) $charsetCollate ";
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta($sql);
+    }
+}
--- services/AssetRepository.php
+++ services/AssetRepository.php
@@ -3,22 +3,11 @@ namespace Serv;
 use Ent\RiskCategory;
 use Ent\Risk;
 use Ent\Assesment;
+use Ent\Customer;
 class AssetRepository {
-
+    
     public static function  create_required_tables() {
-        global $wpdb;
-        $charsetCollate = $wpdb->get_charset_collate();
-        $clientsTableName = $wpdb->prefix . 'customers';
-        $sql = "CREATE TABLE $clientsTableName (
-            company_id      integer NOT NULL AUTO_INCREMENT,
-            company_name    varchar(255),
-            company_email   varchar(255),
-            company_afm     varchar(10),
-            comapny_address varchar(255), 
-            PRIMARY KEY (company_id)
-        ) $charsetCollate ";
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php' );
-        dbDelta($sql);
+        Customer::create_customer_table();
         RiskCategory::create_risk_category_table();
         Risk::create_risk_table();
         Assesment::create_assesment_table();
